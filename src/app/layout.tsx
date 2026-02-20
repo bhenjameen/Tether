6efb5cad37f91d722c3e73542141e2cb2e9ed52e
Benjamin Patrick <bp.eyoh@gmail.com>
2026-02-20 23:02:52 +0100

    chore: trigger deployment after restoration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+// Triggering deployment after system restoration
 
 const inter = Inter({ subsets: ['latin'] })
 
